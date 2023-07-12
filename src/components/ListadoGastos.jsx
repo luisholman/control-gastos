fb3ba3e08ec206afb5ba5cfb9de4afd1db12dde7
luisholman <88738669+luisholman@users.noreply.github.com>
2023-07-12 13:27:18 -0500

    commit

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -15,7 +15,7 @@ export const ListadoGastos = ({
       {
         filtro ? (
           <>
-            <h2>{gastosFiltrados2.length ? 'Gastos' : 'No hay gastos en esta categoria'}</h2>
+            <h2>{gastosFiltrados2.length ? 'Lista Gasto' : 'No hay gastos en esta categoria'}</h2>
               {gastosFiltrados2.map (gasto =>(
 
                 <Gasto
@@ -28,7 +28,7 @@ export const ListadoGastos = ({
           </>
         ) : (
             <>
-            <h2>{gastos.length ? 'Gastos' : 'No hay Gastos'}</h2>
+            <h2>{gastos.length ? 'Listado de Gastos' : 'No hay Gastos'}</h2>
               {gastos.map(gasto => (
 
                 <Gasto
